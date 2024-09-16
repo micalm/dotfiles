9bb571cf22c90c32dd5f2d166ce15dae4500a087
Mateusz Micał <mateusz@mical.pl>
2024-09-16 09:38:48 +0200

    feat: add utilities, fix eza repo

---
+++ utilities/add-eza-repo.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+touch $HOME/.add-eza-repo
